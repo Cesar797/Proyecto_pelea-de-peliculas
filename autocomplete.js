8a6e3b6d27ba6fab4cfa4d47470c61826ef7c7f2
Cesar797 <cesarbelmontes2007@gmail.com>
2025-04-07 18:00:19 -0700

    Add files via upload

--- autocomplete.js
+++ autocomplete.js
@@ -1,59 +1,62 @@
-const createAutocomplete ({root, rederoption, onoptioselect, inputvalue, fetchData}) => {
-    //funcion createAUTocomple
-    root.innerHTML=`
-    <label><b>busqueda</b></label>
-<input class="input" />
-<div class="dropdown">
-<div class="dropdown-menu">
-<div class= "dropdown-content rsults"></div>
-</div>
-</div>
-`
-}
+const createAutoComplete = ({root, renderOption, onOptionSelect, inputValue, fetchData }) => {
 
-const input = root.querySelector(".input")
-const dropdown = root.querySelect(".dropdown")
-const resultswrapper = root.querySelector(".result")
+    root.innerHTML = `
+    <label><b>Busqueda </b></label>
+    <input class="input" />
+    <div class="dropdown">
+        <div class="dropdown-menu">
+        <div class="dropdown-content results"></div>
+        </div>
+    </div>
+    `;
 
-
-const Debonce = (func, delay = 1000 )  =>{
-    lettimeoutid
-    return(...args) => {
-        clearTimeout(timeout)
-        timeout = setTimeout(() => {
-func.apply(null,args)
-        },delay)
-
-}
-}
-
-const onInput = async event => {
-    const items = await fetchData(event.target.value)
-    console.log("movies", items)
-if(!items.length){
-    dropdown.classlist.remove('is-active')
-    for(let item of items){
-        const option = document.createElement('a')
-
-        option.classlist.add('dropdown-item')
-        option.innerHTMl = renderoption(item)
-        option.addEventListener('click', () =>{
-        dropdown.classlist.remove('is-active')
-    input.value = inpuntValue(item)
-onOptionSelect(item)
-console.log("onMovieSelect")
+    const input = root.querySelector('input');
+    const dropdown = root.querySelector('.dropdown');
+    const resultsWrapper = root.querySelector('.results');
 
 
-    })
-    resultswrapper.appendchild(option)
-}
-}
-}
-
-input.addeventlistener('input', debounce (oninput, 1000))
-
-document.addEventlistener('click', event =>{
-    if(!root.contains(event.target)){
-        dropdown.classlist.remove('is-active')
+    const debounce = (func, delay = 1000) => {
+        let timeoutId
+        return(...args) => {
+            clearTimeout(timeoutId)
+            timeoutId = setTimeout(() => {
+                func.apply(null, args)
+            }, delay)
+        }
     }
-})
+    const onInput = async event => {
+        const items = await fetchData(event.target.value)
+        console.log("MOVIES: ", items)
+
+        if(!items.length){
+            dropdown.classList.remove('is-active')
+            return
+        }
+
+        resultsWrapper.innerHTML = ''
+        dropdown.classList.add('is-active')
+        for(let item of items) {
+            // const div = document.createElement('div')
+            const option = document.createElement('a')
+
+            option.classList.add('dropdown-item')
+            option.innerHTML = renderOption(item)            
+            option.addEventListener('click', () => {
+                dropdown.classList.remove('is-active')
+                input.value = inputValue(item)
+                onOptionSelect(item)
+                console.log("onMovieSelect")
+            })
+            // document.querySelector('#target').appendChild(div)
+            resultsWrapper.appendChild(option)
+        }
+    }
+    input.addEventListener('input', debounce(onInput, 1000))
+
+    document.addEventListener('click', event => {
+        if(!root.contains(event.target)) {
+            dropdown.classList.remove('is-active')
+        }
+    })
+
+}
